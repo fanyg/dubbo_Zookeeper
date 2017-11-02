9ebccc6ba2a980f6830babcc2ad975509146bc17
fyg_bxbank <yonggang.fan@pactera.com>
2017-11-02 17:08:16 +0800

    add

---
+++ dubbo-consumer02/src/main/java/com/pactera/controller/TestController.java
@@ -0,0 +1,39 @@
+package com.pactera.controller;
+
+import java.util.Map;  
+
+
+import javax.annotation.Resource;  
+  
+  
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.ResponseBody;  
+  
+  
+import com.pactera.service.TestService;  
+  
+  
+  
+  
+/**  
+ * @author xianghui1  
+ *  
+ *         测试controller  
+ */  
+@Controller  
+public class TestController {  
+  
+  
+    @Resource(name = "testService")  
+    TestService testInterface;  
+  
+  
+    @RequestMapping(value = { "/consumer/testMain" })  
+    @ResponseBody  
+    public String testDubbo() {  
+        return "consuemr01" + testInterface.test();  
+    }  
+  
+  
+}
